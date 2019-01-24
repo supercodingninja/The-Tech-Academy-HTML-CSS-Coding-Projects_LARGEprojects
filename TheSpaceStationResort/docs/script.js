975730762bdb34fd8d9481f03b10b6a681802b04
Rev. Frederick D. Thomas, Jr.; USMC Multiple Wars Vet <frederickdthomasjr@gmail.com>
2019-01-24 05:38:35 -0800

    Better

--- TheSpaceStationResort/docs/script.js
+++ TheSpaceStationResort/docs/script.js
@@ -1,5 +1,44 @@
-// This awesome code can be found at https://gist.github.com/CodeMyUI/97d2ea07e2299c891a68d74f5073e34f ; and the tutorial is at https://codemyui.com/fade-intro-text-animation/  Please feel free to Fork this code; and try your own combinations.//
 $("#os-phrases > h2")
     .css('opacity', 1).lettering( 'words' )
     .children( "span" ).lettering()
     .children( "span" ).lettering(); 
+    
+/* Lettering.JS 0.6.1 by Dave Rupert  - http://daverupert.com */
+(function($){
+    function injector(t,splitter,klass,after){
+        var a=t.text().split(splitter),inject='';
+    if(a.length){
+        $(a).each(function(i,item){
+            inject+='<span class="'+klass+(i+1)+'">'+item+'</span>'+after
+            });
+    t.empty().append(inject)
+        }
+    }
+    var methods={
+        init:function(){
+            return this.each(function(){
+                injector($(this),'','char','')
+            }
+        )},
+        words:function(){
+            return this.each(function(){
+                injector($(this),' ','word',' ')
+            })
+        },lines:function(){
+            return this.each(function(){
+                var r="eefec303079ad17405c889e092e105b0";
+                injector($(this).children("br").replaceWith(r).end(),r,'line','')
+            })
+        }
+    };
+    $.fn.lettering=function(method){
+        if(method&&methods[method]){
+            return methods[method].apply(this,[].slice.call(arguments,1))
+        }
+        else if(method==='letters'||!method){
+            return methods.init.apply(this,[].slice.call(arguments,0))
+        }
+        $.error('Method '+method+' does not exist on jQuery.lettering');
+        return this
+    }
+})(jQuery);
